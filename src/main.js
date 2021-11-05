b9804f25d0f0edc115f5261fe1326b260cfed6b8
Janegro09 <javier.sosa099@gmail.com>
2021-11-05 18:47:26 -0300

    se suma env

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ const { routerProductos } = require('./routes/productos')
 const { routerCarrito } = require('./routes/carrito')
 
 
-const port = 8080;
+const port = process.env.PORT | 8080;
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('../public'));
